04b286edd104a8f1a99be702200e8bd1889d3b2f
hsinmingtsi <hsinmingtsi@gmail.com>
2022-08-14 16:59:01 +0800

    test

---
+++ pika.py
@@ -0,0 +1,37 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # test
+
+# %% [markdown]
+# ## test2
+
+# %% [markdown]
+# ### test3
+
+# %%
+import string
+a = 3
+print(f'{a}, test test test')
+
+# %% [raw]
+# dddddd
+
+# %% [markdown]
+# facebook
+
+# %%
+fajli;oljiofjieoa';
